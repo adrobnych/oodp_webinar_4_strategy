fd787b4cb0620362a0389ef35345556ca216f96c
Aleksey Drobnych <adrobnych@gmail.com>
2014-04-15 15:00:48 +0300

    after refactoring

--- CompensationCalculator/src/com/droidbrew/oodp/stategy/PayrollDay.java
+++ CompensationCalculator/src/com/droidbrew/oodp/stategy/PayrollDay.java
@@ -1,62 +1,49 @@
 package com.droidbrew.oodp.stategy;
 
 public enum PayrollDay {
-	MONDAY {
-		@Override
-		protected double overtimePay(double hours, double rate) {
-			return hours <= HOURS_PER_SHIFT ? 0 :
-				(hours - HOURS_PER_SHIFT) * rate;
-		}
-	}, 
-	TUESDAY {
-		@Override
-		protected double overtimePay(double hours, double rate) {
-			return hours <= HOURS_PER_SHIFT ? 0 :
-				(hours - HOURS_PER_SHIFT) * rate;
-		}
-	}, 
-	WEDNESDAY {
-		@Override
-		protected double overtimePay(double hours, double rate) {
-			return hours <= HOURS_PER_SHIFT ? 0 :
-				(hours - HOURS_PER_SHIFT) * rate;
-		}
-	}, 
-	THURSDAY {
-		@Override
-		protected double overtimePay(double hours, double rate) {
-			return hours <= HOURS_PER_SHIFT ? 0 :
-				(hours - HOURS_PER_SHIFT) * rate;
-		}
-	}, 
-	FRIDAY {
-		@Override
-		protected double overtimePay(double hours, double rate) {
-			return hours <= HOURS_PER_SHIFT ? 0 :
-				(hours - HOURS_PER_SHIFT) * rate;
-		}
-	}, 
-	SATURDAY {
-		@Override
-		protected double overtimePay(double hours, double rate) {
-			return hours*rate;
-		}
-	},
-	SUNDAY {
-		@Override
-		protected double overtimePay(double hours, double rate) {
-			return hours*rate;
-		}
-	};
+	MONDAY(PayType.WEEKDAY), 
+	TUESDAY(PayType.WEEKDAY), 
+	WEDNESDAY(PayType.WEEKDAY), 
+	THURSDAY(PayType.WEEKDAY), 
+	FRIDAY(PayType.WEEKDAY), 
+	SATURDAY(PayType.WEEKENDDAY),
+	SUNDAY(PayType.WEEKENDDAY);
 	
-	private static final int HOURS_PER_SHIFT = 8;
+	public enum PayType{
+		WEEKDAY {
+			@Override
+			protected double overtimePay(double hours, double rate) {
+				return hours <= HOURS_PER_SHIFT ? 0 :
+					(hours - HOURS_PER_SHIFT) * rate;
+			}
+		},
+		WEEKENDDAY {
+			@Override
+			protected double overtimePay(double hours, double rate) {
+				return hours*rate;
+			}
+		};
+		
+		private static final int HOURS_PER_SHIFT = 8;
 
-	public double pay(double hours, double rate) {
-		if(hours < 0 || rate < 0 || hours > 24)
-			return -1;
-		return hours*rate + overtimePay(hours, rate);
+		public double pay(double hours, double rate) {
+			if(hours < 0 || rate < 0 || hours > 24)
+				return -1;
+			return hours*rate + overtimePay(hours, rate);
+		}
+
+		
+		abstract protected double overtimePay(double hours, double rate);
+		
+	}
+	
+	private PayType payType = null;
+	
+	public PayType getPayType() {
+		return payType;
 	}
 
-	
-	abstract protected double overtimePay(double hours, double rate);
+	PayrollDay(PayType pt){
+		this.payType = pt;
+	}
 }
--- CompensationCalculator/src/com/droidbrew/oodp/stategy/spec/DailyCompensations.java
+++ CompensationCalculator/src/com/droidbrew/oodp/stategy/spec/DailyCompensations.java
@@ -10,38 +10,38 @@ public class DailyCompensations {
 
 	@Test
 	public void testShiftSalaryOnWorkingDay(){
-		assertEquals(60, PayrollDay.MONDAY.pay(6, 10), 0.0001);
+		assertEquals(60, PayrollDay.MONDAY.getPayType().pay(6, 10), 0.0001);
 	}
 	
 	@Test
 	public void testShiftSalaryOnWeekend(){
-		assertEquals(120, PayrollDay.SUNDAY.pay(6, 10), 0.0001);
+		assertEquals(120, PayrollDay.SUNDAY.getPayType().pay(6, 10), 0.0001);
 	}
 	
 	@Test
 	public void testOvertimeSalaryOnWorkingDay(){
-		assertEquals(120, PayrollDay.TUESDAY.pay(10, 10), 0.0001);
+		assertEquals(120, PayrollDay.TUESDAY.getPayType().pay(10, 10), 0.0001);
 	}
 	
 	@Test
 	public void testOvertimeSalaryOnWeekend(){
-		assertEquals(200, PayrollDay.SATURDAY.pay(10, 10), 0.0001);
+		assertEquals(200, PayrollDay.SATURDAY.getPayType().pay(10, 10), 0.0001);
 	}
 	
 	@Test
 	public void testNegativeInput(){
-		assertEquals(-1, PayrollDay.SATURDAY.pay(-10, 10), 0.0001);
-		assertEquals(-1, PayrollDay.WEDNESDAY.pay(-10, 10), 0.0001);
+		assertEquals(-1, PayrollDay.SATURDAY.getPayType().pay(-10, 10), 0.0001);
+		assertEquals(-1, PayrollDay.WEDNESDAY.getPayType().pay(-10, 10), 0.0001);
 	}
 	
 	@Test
 	public void testZeroInput(){
-		assertEquals(0, PayrollDay.THURSDAY.pay(0, 0), 0.0001);
+		assertEquals(0, PayrollDay.THURSDAY.getPayType().pay(0, 0), 0.0001);
 	}
 	
 	@Test
 	public void testGT24HoursInput(){
-		assertEquals(-1, PayrollDay.FRIDAY.pay(25, 10), 0.0001);
+		assertEquals(-1, PayrollDay.FRIDAY.getPayType().pay(25, 10), 0.0001);
 	}
 	
 	
